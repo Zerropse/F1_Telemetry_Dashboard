eb392cfa0d0a162295f8fdc98bfa803f1e16cfff
Kush Singh <123228529+Zerropse@users.noreply.github.com>
2026-02-07 11:35:15 +0530

    Update telemetry.py

--- telemetry.py
+++ telemetry.py
@@ -1,7 +1,11 @@
+import os
 import fastf1
 import pandas as pd
 
-fastf1.Cache.enable_cache('cache')
+cache_dir = "/tmp/fastf1_cache"
+os.makedirs(cache_dir, exist_ok=True)
+fastf1.Cache.enable_cache(cache_dir)
+
 
 def get_telemetry(year, track, session_type, driver_code):
     session = fastf1.get_session(year, track, session_type)
